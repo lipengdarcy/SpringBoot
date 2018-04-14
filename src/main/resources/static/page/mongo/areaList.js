3a1120454d26366e95f824085163c6fc1be943ba
darcy <darcy@LIPENG>
2018-04-14 17:18:21 +0800

    增加mongodb的grid数据展示

---
+++ src/main/resources/static/page/mongo/areaList.js
@@ -0,0 +1,50 @@
+$(function() {
+	var gridid = 'grid_table';
+	var grid_selector = "#" + gridid;
+	var pager_selector = "#grid_pager";
+
+	$(grid_selector).jqGrid({
+		url : 'areaData',
+		datatype : "json",
+		colNames : [ '编号', '上级编号', '区域名称', '省', '市', '区','级别' ],
+		colModel : [ {
+			name : 'id'
+		}, {
+			name : "pid"
+		}, {
+			name : "name"
+		}, {
+			name : "provinceName"
+		}, {
+			name : "cityName"
+		}, {
+			name : "areaName"
+		}, {
+			name : "level"
+		} ],
+
+		viewrecords : true,
+		autowidth : true, // 宽度自适应
+		height : 'auto', // 高度自适应
+		rownumbers : true, // 显示序号
+		rownumWidth : 60,// 序号列宽度
+		rowNum : 10,
+		rowList : [ 10, 20, 30 ],
+		//caption: "行政区域列表",
+		pager : pager_selector
+	});
+
+});
+
+// 查询
+function query() {
+	var data = {
+		updateBegin : $("#form input[name='updateBegin'").val(),
+		updateEnd : $("#form input[name='updateEnd'").val(),
+		type : $("#type").val()
+	};
+	var postData = $("#grid_table").jqGrid("getGridParam", "postData");
+	// 将查询参数融入postData选项对象
+	$.extend(postData, data);
+	$("#grid_table").jqGrid('setGridParam').trigger("reloadGrid");
+}
